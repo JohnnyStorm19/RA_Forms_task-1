614b8a1368e2e7a16c996f182921643a481ed91e
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-11-16 12:51:46 +0300

    vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: 'https://johnnystorm19.github.io/RA_Props-TS_task-3/'
+  base: 'https://johnnystorm19.github.io/RA_Forms_task-1/'
 })
